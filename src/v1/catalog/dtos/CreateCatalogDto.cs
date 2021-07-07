7c093430a7bcc2ee374aa69f0ff8788623585863
Hasan Ahamed <officials.hasan@gmail.com>
2021-07-08 02:45:14 +0600

    7.2 HTTP[POST] add dto annotation

--- src/v1/catalog/dtos/CreateCatalogDto.cs
+++ src/v1/catalog/dtos/CreateCatalogDto.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Catalog.Dtos
 {
     public record createItemDto
     {
+        [Required]
         public string name { get; init; }
 
+        [Required]
+        [Range(1, 1000)]
         public decimal price { get; init; }
     }
 }
